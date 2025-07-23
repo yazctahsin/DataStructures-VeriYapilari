9e07f805ca0e0e6ec2a5169f7bdcf5abe2c2384a
Tahsin Yazıcı <117100542+yazctahsin@users.noreply.github.com>
2025-07-23 21:13:41 +0300

    Create day01.py

---
+++ day01.py
@@ -0,0 +1,20 @@
+def wordCounter(sentece):
+    wordCount = sentence.split(" ")
+    print(wordCount)
+
+sentence = input("Please enter the text: ")
+wordCounter(sentence)
+
+
+def swapVariables(var1, var2)
+    temp = var1
+    var1 = var2
+    var2 = temp
+    print(f"var1 = {var1} var2 = {var2}")
+
+
+
+var1 = int(input("Enter the first Value: ")
+var2 = int(input("Enter the second Value: ")
+
+swapVariables(var1, var2)
