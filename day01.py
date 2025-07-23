4a0de8a713a65702ae7cb956cdf7bc0c863f9c80
Tahsin Yazıcı <117100542+yazctahsin@users.noreply.github.com>
2025-07-23 21:18:21 +0300

    Update day01.py

--- day01.py
+++ day01.py
@@ -18,3 +18,18 @@ var1 = int(input("Enter the first Value: ")
 var2 = int(input("Enter the second Value: ")
 
 swapVariables(var1, var2)
+
+
+
+#day002 Python variables
+
+name = "John"
+age = 23
+avgscr = 78
+
+
+str1 = "Hello"
+str2 = " world!"
+
+str3 = str1+str2
+print(str3)
